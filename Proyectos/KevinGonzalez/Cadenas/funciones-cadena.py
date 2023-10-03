f78edf14218582ce448feae2c40a04a3d0b55974
Johan González <soyelsxbbsx@outlook.com>
2023-10-02 23:24:17 -0500

    Atascado en ejercicio de telefono(tengo sueño)

--- Proyectos/KevinGonzalez/Cadenas/funciones-cadena.py
+++ Proyectos/KevinGonzalez/Cadenas/funciones-cadena.py
@@ -1,9 +1,13 @@
 # Ejemplo de como imprimir los caracteres de una cada en cada linea, siempre y cuando el valor de la banderilla sea menor a la longitud de la cadena
-s = input ("Ingrese string: ")
-j = 0
-while j < len(s):
-    print(s[j])
-    j += 1
+# s = input("Ingrese string: ")
+# j = 0
+# while j < len(s):
+#     print(s[j])
+#     j += 1
 
-#ejercicio 1
+# ejercicio 1
+telefono = input("////////Ingrese telefono con el formato +34-XXXXXXXXX-XX////////\n")
 
+telefono.find("-")
+telefono.rfind("-")
+print(telefono.find, telefono.rfind)
