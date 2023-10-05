081ce2c0596660a0da966e6670a4dea4060a2460
Johan González <soyelsxbbsx@outlook.com>
2023-10-04 23:36:03 -0500

    add def facturaciones

--- Proyectos/KevinGonzalez/Funciones/ejer-presentacion.py
+++ Proyectos/KevinGonzalez/Funciones/ejer-presentacion.py
@@ -1,9 +1,3 @@
-N = 0
-
-Name = input("Nombre del usuario: ")
-Strat = int(input("De su estrato (1-5): "))
-Impulse = input("Impulsos del mes: ")
-
 def facturaciones(Strat,Impulse):
     if Strat == 1:
         tarifa = 10000
@@ -13,8 +7,20 @@ def facturaciones(Strat,Impulse):
         tarifa = 20000
     elif Strat == 4:
         tarifa = 25000
-    elif Strat == 5:
+    else:
         tarifa = 30000
     ImpulseValue = Impulse * 100
     subsValue = tarifa + ImpulseValue
-    return subsValue
+    return subsValue   
+
+N = int(input("Cantidad de abonados: "))
+TotalPaySubs = 0
+for k in range(N):
+    Name = input("Nombre del usuario: ")
+    Strat = int(input("De su estrato (1-5): "))
+    Impulse = int(input("Impulsos del mes: "))
+    subsValue = facturaciones(Strat,Impulse)
+    TotalPaySubs += subsValue
+    print("*" * 80,"\nUsuario: ", Name, "\nTotal a pagar: ", subsValue)
+print("*" * 80,"\nTotal a pagar de todos los abonados: ", TotalPaySubs)
+
