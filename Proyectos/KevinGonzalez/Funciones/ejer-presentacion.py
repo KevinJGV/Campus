3591d274ba9e3ff0bd7b003e3a61ad48123c13a3
Johan González <soyelsxbbsx@outlook.com>
2023-10-08 23:48:24 -0500

    adicionado funcion* "ValidarStrat" a ejercicio*

--- Proyectos/KevinGonzalez/Funciones/ejer-presentacion.py
+++ Proyectos/KevinGonzalez/Funciones/ejer-presentacion.py
@@ -11,16 +11,30 @@ def facturaciones(Strat,Impulse):
         tarifa = 30000
     ImpulseValue = Impulse * 100
     subsValue = tarifa + ImpulseValue
-    return subsValue   
+    return subsValue, tarifa, ImpulseValue
+
+
+def ValidarStrat(dato):
+    while True:
+        try:
+            Strat = int(input(dato))
+            if 1 < Strat > 5:
+                print("Ingrese un dato valido solicitado.")
+                continue
+            break
+        except ValueError:
+            print("Error. Ingrese un numero entero.")
+    return Strat
 
 N = int(input("Cantidad de abonados: "))
 TotalPaySubs = 0
 for k in range(N):
     Name = input("Nombre del usuario: ")
-    Strat = int(input("De su estrato (1-5): "))
+    Strat = ValidarStrat("Dé su estrato (1-5): ")
     Impulse = int(input("Impulsos del mes: "))
-    subsValue = facturaciones(Strat,Impulse)
+    subsValue, tarifa, ImpulseValue = facturaciones(Strat,Impulse)
+
     TotalPaySubs += subsValue
-    print("*" * 80,"\nUsuario: ", Name, "\nTotal a pagar: ", subsValue)
+    print("*" * 80,"\nUsuario: ", Name,"\n Tarifa: ", tarifa, "\nTotal a pagar: ", subsValue)
 print("*" * 80,"\nTotal a pagar de todos los abonados: ", TotalPaySubs)
 
