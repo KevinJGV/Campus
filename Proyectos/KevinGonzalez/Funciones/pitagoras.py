44743442eb418cfce8469aa4921ef58973287204
Johan González <85241471+KevinJGV@users.noreply.github.com>
2023-10-17 21:41:10 +0000

    Completo primer punto

--- Proyectos/KevinGonzalez/Funciones/pitagoras.py
+++ Proyectos/KevinGonzalez/Funciones/pitagoras.py
@@ -7,7 +7,7 @@ def posS(Xs,Ys):
 
 def length(posT,posS):
     d = math.sqrt((posT[0] - posS[0] )**2 + (posT[1] - posS[1])**2)
-    return "La distancia entre estos puntos es: ",d
+    return f"La distancia entre estos puntos es: {d:.2f}"
 
 print("\tCALCULADOR DE TEOREMA DE PITAGORAS\n")
 
@@ -17,4 +17,5 @@ Xs = float(input("Ingrese la posicion en (formato float) X de s: "))
 Ys = float(input("Ingrese la posicion en (formato float) Y de s: "))
 posT(Xt,Yt)
 posS(Xs,Ys)
-length(posT,posS)
+d = length(posT(Xt, Yt), posS(Xs, Ys))
+print(d)
