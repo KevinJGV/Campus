59a6503bef86bcaec255634b7333f6bea55dde16
Johan González <85241471+KevinJGV@users.noreply.github.com>
2023-10-17 15:50:07 -0500

    Reto del onedrive de C4

---
+++ Proyectos/KevinGonzalez/Funciones/pitagoras.py
@@ -0,0 +1,20 @@
+import math
+def posT(Xt,Yt):
+    return Xt,Yt
+
+def posS(Xs,Ys):
+    return Xs,Ys
+
+def length(posT,posS):
+    d = math.sqrt((posT[0] - posS[0] )**2 + (posT[1] - posS[1])**2)
+    return "La distancia entre estos puntos es: ",d
+
+print("\tCALCULADOR DE TEOREMA DE PITAGORAS\n")
+
+Xt = float(input("Ingrese la posicion en (formato float) X de t: "))
+Yt = float(input("Ingrese la posicion en (formato float) Y de t: "))
+Xs = float(input("Ingrese la posicion en (formato float) X de s: "))
+Ys = float(input("Ingrese la posicion en (formato float) Y de s: "))
+posT(Xt,Yt)
+posS(Xs,Ys)
+length(posT,posS)
