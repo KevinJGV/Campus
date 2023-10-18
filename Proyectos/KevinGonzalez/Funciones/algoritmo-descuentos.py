e889b8c05454b4da46ed2d896b1c9b51bf3c6e2b
Johan González <soyelsxbbsx@outlook.com>
2023-10-17 20:23:38 -0500

    Pequeña correcion

--- Proyectos/KevinGonzalez/Funciones/algoritmo-descuentos.py
+++ Proyectos/KevinGonzalez/Funciones/algoritmo-descuentos.py
@@ -3,14 +3,18 @@
 
 
 def funcion(valorDcto):
-    valorStr = input("Ingrese el costo de su compra: ")
-    valor = int(valorStr)
-    while valorStr.isdigit():
-        if valor > 150000:
-            valorDcto = valor * 0.05
-            return print(f"El descuento correspondiente a su compra es de {valorDcto}")
-        else:
-            return print("No corresponde al descuento del 5%")
-
-valorDcto = ()
+    while True:
+        try:
+            valorStr = input("Ingrese el costo de su compra: ")
+            valor = int(valorStr)
+            if valor > 150000:
+                valorDcto = valor * 0.05
+                return print(f"El descuento correspondiente a su compra es de {valorDcto}")
+            else:
+                return print("No corresponde al descuento del 5%")
+            break
+        except ValueError:
+            print("Ingrese un valor numerico.")
+            input()
+valorDcto = []
 funcion(valorDcto)
