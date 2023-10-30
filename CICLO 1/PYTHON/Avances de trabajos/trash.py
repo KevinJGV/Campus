8a2ed8c0f8d7fee93d9e6a7857cef86d4f7428e1
Johan González <85241471+KevinJGV@users.noreply.github.com>
2023-10-30 15:47:40 -0500

    Archivo de "avance" para Acme-menu

---
+++ CICLO 1/PYTHON/Avances de trabajos/trash.py
@@ -0,0 +1,8 @@
+def ordenar_key(dic):
+    for clave,valor in dic.items():
+        print(clave,valor)
+
+listado = {"1": {"id": "1", "nombre": "asd", "precio": "1.0", "cantidad": "1"}, "2": {"id": "2", "nombre": "sdf", "precio": "2.0", "cantidad": "2"}, "3": {"id": "3", "nombre": "dfg", "precio": "3.0", "cantidad": "3"},"asdf": {"id": "1", "nombre": "asd", "precio": "1.0", "cantidad": "1"}}
+
+a= ordenar_key(listado)
+print(a)
