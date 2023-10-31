3e48c335c728dc111c8151a2acfdc7a315b5ae35
Johan González <soyelsxbbsx@outlook.com>
2023-10-30 20:51:12 -0500

    HTML SEMANTICO

--- CICLO 1/PYTHON/Avances de trabajos/trash.py
+++ CICLO 1/PYTHON/Avances de trabajos/trash.py
@@ -4,5 +4,6 @@ def ordenar_key(dic):
 
 listado = {"1": {"id": "1", "nombre": "asd", "precio": "1.0", "cantidad": "1"}, "2": {"id": "2", "nombre": "sdf", "precio": "2.0", "cantidad": "2"}, "3": {"id": "3", "nombre": "dfg", "precio": "3.0", "cantidad": "3"},"asdf": {"id": "1", "nombre": "asd", "precio": "1.0", "cantidad": "1"}}
 
+list_ado = list()
 a= ordenar_key(listado)
 print(a)
