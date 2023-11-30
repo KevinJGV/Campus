0bdcbc65a2b5beccaafd97baffab5d3e13766e31
Johan González <soyelsxbbsx@outlook.com>
2023-11-29 21:39:35 -0500

    Inicio de JavaScript

---
+++ CICLO 2/JavaScript/Intro/EjemploJs.js
@@ -0,0 +1,8 @@
+console.log("      *  ***** \n      * *     *\n      * *      \n      *  ***** \n*     *        \n*     * *     *\n ***** ***** ")
+
+// ESTE ES UN COMENTARIO DE UNA LINEA
+
+/* ESTE
+ES UNO
+MULTILINEA
+*/
