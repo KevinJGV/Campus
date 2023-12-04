3fd5c35b5ac268ad7986ae2f5749f1844047fb7a
Johan González <soyelsxbbsx@outlook.com>
2023-12-03 19:33:34 -0500

    Ejercicio 5

---
+++ CICLO 2/Parte-2/JavaScript/SoftwareReviews/Primero/Ejercicio5.js
@@ -0,0 +1,5 @@
+let d =  Number(prompt("CALCULADOR DE TIEMPO VIAJANDO A 40MPH\nIngrese la distancia que medira en millas"));
+
+let v = 40/60; let t = d/v; t = Number(t.toFixed(1));
+
+console.log(t)
