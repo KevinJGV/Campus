d7bb7c7b49ab2e26282e41ff63e61c20e7e48c39
Kevin González <soyelsxbbsx@outlook.com>
2023-12-04 15:28:57 -0500

    Solved

--- CICLO 2/Parte-2/JavaScript/SoftwareReviews/Tercero/Ejercicio1.js
+++ CICLO 2/Parte-2/JavaScript/SoftwareReviews/Tercero/Ejercicio1.js
@@ -1,9 +1,9 @@
-function validar(msj) {
+function validartext(msj) {
   let flag = true;
   debugger
   input = prompt(msj)
   while (flag === true) {
-    if (input == 0 || isNaN(input) || input === "") {
+    if (input === 0 || !isNaN(input) || input === "") {
       alert("Error en los datos ingresados, ingreselo nuevamente");
       input = prompt(msj);
     } else {
@@ -13,11 +13,26 @@ function validar(msj) {
   }
 }
 
+function validarnum(msj) {
+    let flag = true;
+    debugger
+    input = Number(prompt(msj))
+    while (flag === true) {
+      if (input === 0 || isNaN(input) || input === "") {
+        alert("Error en los datos ingresados, ingreselo nuevamente");
+        input = prompt(msj);
+      } else {
+        flag = false;
+        return input;
+      }
+    }
+  }
+
 let flag = true;
 while (flag === true) {
-  prompt1 = validar("CALCULADORA DE PRECIO TOTAL\nNombre del articulo");
-  prompt2 = validar("CALCULADORA DE PRECIO TOTAL\nPrecio individual del articulo");
-  prompt3 = validar("CALCULADORA DE PRECIO TOTAL\nCantidad del articulo");
+  prompt1 = validartext("CALCULADORA DE PRECIO TOTAL\nNombre del articulo");
+  prompt2 = validarnum("CALCULADORA DE PRECIO TOTAL\nPrecio individual del articulo");
+  prompt3 = validarnum("CALCULADORA DE PRECIO TOTAL\nCantidad del articulo");
   total = prompt3 * prompt2
   alert(`FACTURA\t\nArticulo: ${prompt1}\nPrecio individual: ${prompt2}\nCantidad: ${prompt3}\nPrecio a pagar(sin IVA): ${total}`)
 
