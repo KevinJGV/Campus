b2bcaf87ab98f06fe0fe08fd8c7e5da744409429
Kevin Gonzalez <soyelsxbbsx@outlook.com>
2023-12-05 01:24:07 -0500

    tema de clase

---
+++ CICLO 2/Parte-2/JavaScript/Arrays/recorrido.js
@@ -0,0 +1,13 @@
+let frutas = ["Pera","Manzana", "Mandarina", "Banano"]
+
+// Recorrido por indices
+
+// for (let i=0; i<frutas.length;i++){
+//     console.log(frutas[i]);
+//     }
+
+//Recorrido por ejementos
+
+for (const fruta of frutas) {
+    console.log(fruta);
+}
