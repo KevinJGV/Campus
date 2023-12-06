704ceb4f6d864c3031e8585768d1f9a0f0243d5e
Johan González <soyelsxbbsx@outlook.com>
2023-12-06 00:43:23 -0500

    Cuarto SR Desarrollado

--- CICLO 2/Parte-2/JavaScript/SoftwareReviews/Cuarto/Ejercicio.js
+++ CICLO 2/Parte-2/JavaScript/SoftwareReviews/Cuarto/Ejercicio.js
@@ -1,19 +1,55 @@
-function o(num,concat) {
-    let r = Number(String(num).repeat(concat))
-    console.log(r)
-}
-let suma = 0
-function superdigit(num) {
-    let l = num.length
-    if (l > 1){
-    for (let i = 0; i < l; i++) {
-        suma += Number(num[i])
-    }} else {
-        console.log(num)
-        suma = 0
+function contains(num) {
+    let targets = ["**","*","/","-","+",]
+    let flag = false
+    let op = undefined
+    for (let i = 0; i < targets.length; i++) {
+        if (num.includes(targets[i])) {
+            op = targets[i]
+            flag = true
+            break
+        } 
     }
-   return suma
+    if (flag === true) {
+        let arr = num.split(op);
+        let num1 = parseFloat(arr[0]);
+        let num2 = parseFloat(arr[1]);
+        switch (op) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            case "/":
+                return num1 / num2;
+            case "**":
+                return num1 ** num2;
+        }
+    } else {
+        return num
+    }
 }
 
-console.log(superdigit("8"))
-o(958,5)
+function makest(num,concat) {
+    let r = contains(num)
+    r = String(r).repeat(Number(concat))
+    return r
+}
+
+function superdigit(num, concat) {
+    // debugger
+    let r = makest(num,concat)
+    let l = r.length
+    let suma = 0
+    if (l > 1){
+    for (let i = 0; i < l; i++) {
+        suma += Number(r[i])
+    }} else {
+        console.log(r)
+        suma = 0
+    }
+   return console.log(suma)
+}
+
+console.log(superdigit("1**500","8"))
+
