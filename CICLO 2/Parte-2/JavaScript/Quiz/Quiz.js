1dde2c0ae2afe63a9c2284c8cb6631bbdcbdf7cf
Kevin Gonzalez <soyelsxbbsx@outlook.com>
2023-12-06 20:41:15 -0500

    creacion de jeje

--- CICLO 2/Parte-2/JavaScript/Quiz/Quiz.js
+++ CICLO 2/Parte-2/JavaScript/Quiz/Quiz.js
@@ -5,30 +5,31 @@ const input_interes = Number(prompt("Interés% (Anual)"));
 const input_pagos = Number(prompt("Número de pagos (Mensuales)"));
 
 
-
-
 function proceso(prestamo,interes,cuotas) {
     //Definicion de datos valor
-    debugger
+//    debugger
     let interes_a = prestamo*interes;
     let interes_m = interes_a/cuotas; interes_m = Number(interes_m.toFixed(0))
     let capital = prestamo/cuotas; capital = Number(capital.toFixed(0))
     let total_deuda = prestamo+interes_a;
     let deuda_mensual = capital+interes_m;
     //Bases de datos
-    const map = new Map()
-    const bdd = new Array(prestamo, total_deuda, capital, interes_m, deuda_mensual)
+    const map = new Map();
+    const bdd = new Array(prestamo, total_deuda, capital, interes_m, deuda_mensual);
+
     for (let i = 1; i < cuotas+1;i++) {
         if (i == 1) {
-            map.set(i, bdd)
+            let bdd_copia = bdd.slice();
+            map.set(i, bdd_copia);
         } else {
-            bdd[0] = bdd[0] - capital
-            bdd[1] = bdd[1] - deuda_mensual
-            map.set(i, bdd)
+            bdd[0] = bdd[0] - capital;
+            bdd[1] = bdd[1] - deuda_mensual;
+            let bdd_copia = bdd.slice();
+            map.set(i, bdd_copia);
         }
-        
-    }
-    console.log(map)
+    };
+    console.log(map);
+
 }
 
 proceso(input_prestamo,input_interes,input_pagos)
