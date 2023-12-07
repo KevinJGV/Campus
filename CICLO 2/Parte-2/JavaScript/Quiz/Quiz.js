31786a3637360a666aa217b229cf4a3852f940f7
Kevin Gonzalez <soyelsxbbsx@outlook.com>
2023-12-06 20:10:11 -0500

    Avance

--- CICLO 2/Parte-2/JavaScript/Quiz/Quiz.js
+++ CICLO 2/Parte-2/JavaScript/Quiz/Quiz.js
@@ -1,19 +1,34 @@
 // Inicializacion de datos
 
-let input_prestamo = prompt("Simulador de prestamo\n\nMonto del credito");
-let input_interes = prompt("Interés% (Anual)");
-let input_pagos = prompt("Número de pagos (Mensuales)");
-let interes_a = input_prestamo*input_interes;
-let interes_m = interes_a/input_pagos;
-let capital = input_prestamo/input_pagos;
-let total_deuda = input_prestamo+interes_a;
-let deuda_mensual = capital+interes_m;
+const input_prestamo = Number(prompt("Simulador de prestamo\n\nMonto del credito"));
+const input_interes = Number(prompt("Interés% (Anual)"));
+const input_pagos = Number(prompt("Número de pagos (Mensuales)"));
 
-let bdd = new Map()
+
+
 
 function proceso(prestamo,interes,cuotas) {
-    let map = new Map()
-    for (let i = 1; i == cuotas;i++) {
-        return 
+    //Definicion de datos valor
+    debugger
+    let interes_a = prestamo*interes;
+    let interes_m = interes_a/cuotas; interes_m = Number(interes_m.toFixed(0))
+    let capital = prestamo/cuotas; capital = Number(capital.toFixed(0))
+    let total_deuda = prestamo+interes_a;
+    let deuda_mensual = capital+interes_m;
+    //Bases de datos
+    const map = new Map()
+    const bdd = new Array(prestamo, total_deuda, capital, interes_m, deuda_mensual)
+    for (let i = 1; i < cuotas+1;i++) {
+        if (i == 1) {
+            map.set(i, bdd)
+        } else {
+            bdd[0] = bdd[0] - capital
+            bdd[1] = bdd[1] - deuda_mensual
+            map.set(i, bdd)
+        }
+        
     }
+    console.log(map)
 }
+
+proceso(input_prestamo,input_interes,input_pagos)
