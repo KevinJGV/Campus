321ccc562802ede28b8e934005ab12f7b20e7205
Johan González <soyelsxbbsx@outlook.com>
2023-12-18 01:53:59 -0500

    Avance casi de la totalidad del quiz 1

--- CICLO 2/Parte-2/JavaScript/Quiz/1/Quiz.js
+++ CICLO 2/Parte-2/JavaScript/Quiz/1/Quiz.js
@@ -2,40 +2,151 @@
 
 function proceso(prestamo, interes, cuotas) {
     //Definicion de datos valor
-    //    debugger
+    debugger
     let interes_a = prestamo * interes;
-    let interes_m = interes_a / cuotas; interes_m = Number(interes_m.toFixed(0))
-    let capital = prestamo / cuotas; capital = Number(capital.toFixed(0))
+    let interes_m = Number((interes_a / cuotas).toFixed(0));
+    let capital = Number((prestamo / cuotas).toFixed(0));
     let total_deuda = prestamo + interes_a;
     let deuda_mensual = capital + interes_m;
-    //Bases de datos
-    const map = new Map();
-    const bdd = new Array(prestamo, total_deuda, capital, interes_m, deuda_mensual);
 
-    for (let i = 1; i < cuotas + 1; i++) {
-        if (i == 1) {
-            let tabla = document.getElementById("tabla2").innerHTML = "<table><thead><tr><th>MES</th><th>VALOR</th><th>VALOR TOTAL</th><th>CAPITAL</th><th>INTERES</th><th>TOTAL</th></tr></thead></table>"
-            let bdd_copia = bdd.slice();
-            map.set(i, bdd_copia);
-
-        } else {
-            bdd[0] = bdd[0] - capital;
-            bdd[1] = bdd[1] - deuda_mensual;
-            let bdd_copia = bdd.slice();
-            map.set(i, bdd_copia);
-        }
+    //Tabla 1 en la web
+    const tabla1 = (prestamo, interes, cuotas, total_deuda, deuda_mensual) => {
+        const tabla1 = document.querySelector("#tabla1");
+        const fragmento = document.createDocumentFragment();
+        const table = document.createElement("table");
+        const tabla1map = new Map([["Monto del credito", prestamo],
+        ["Tasa de interés (anual)", interes],
+        ["Número de pagos (Mensuales)", cuotas],
+        ["Valor total", total_deuda],
+        ["Valor mensual", deuda_mensual]]);
+        // Escritura en html
+        tabla1map.forEach((valor, clave) => {
+            const tr = document.createElement("tr");
+            const td1 = document.createElement("td");
+            td1.insertAdjacentText("afterbegin", clave);
+            tr.insertAdjacentElement("afterbegin", td1);
+            const td2 = document.createElement("td");
+            td2.insertAdjacentText("afterbegin", valor);
+            tr.insertAdjacentElement("beforeend", td2);
+            table.insertAdjacentElement("beforeend", tr)
+        });
+        fragmento.insertAdjacentElement("afterbegin", table)
+        tabla1.insertAdjacentElement("afterbegin", fragmento)
     };
-    console.log(map);
 
+    tabla1(prestamo, interes, cuotas, total_deuda, deuda_mensual);
+
+    //Tabla 2 en la web
+    const tabla2 = (prestamo, total_deuda, capital, interes_m, interes_a) => {
+        //Preparacion de tabla
+        const tabla2 = document.querySelector("#tabla2")
+        const fragmento = document.createDocumentFragment();
+        const table = document.createElement("table");
+        //Encabezado
+        const thead = document.createElement("thead");
+        let row = document.createElement("tr");
+        for (let i = 0; i < 6; i++) {
+            let th = document.createElement("th");
+            if (i == 0) {
+                th.insertAdjacentText("afterbegin", "Mes");
+                row.insertAdjacentElement("beforeend", th)
+            } else if (i == 1) {
+                th.insertAdjacentText("afterbegin", "Valor");
+                row.insertAdjacentElement("beforeend", th)
+            } else if (i == 2) {
+                th.insertAdjacentText("afterbegin", "Valor total");
+                row.insertAdjacentElement("beforeend", th)
+            } else if (i == 3) {
+                th.insertAdjacentText("afterbegin", "Capital");
+                row.insertAdjacentElement("beforeend", th)
+            } else if (i == 4) {
+                th.insertAdjacentText("afterbegin", "Interes");
+                row.insertAdjacentElement("beforeend", th)
+            } else {
+                th.insertAdjacentText("afterbegin", "Total");
+                row.insertAdjacentElement("beforeend", th)
+            }
+        }
+        thead.insertAdjacentElement("afterbegin", row)
+        table.insertAdjacentElement("beforeend", thead)
+        //Cuerpo
+        const map = new Map();
+        const bdd = new Array(prestamo, total_deuda, capital, interes_m, deuda_mensual);
+
+        for (let itera = 1; itera < cuotas + 1; itera++) {
+            if (itera == 1) {
+                //Cuerpo de la tabla
+                let bdd_copia = bdd.slice();
+                map.set(itera, bdd_copia);
+                let tbody = document.createElement("tbody");
+                let row = document.createElement("tr");
+                for (let i = 0; i < 6; i++) {
+                    let th = document.createElement("th");
+                    if (i == 0) {
+                        th.insertAdjacentText("afterbegin", itera);
+                        row.insertAdjacentElement("beforeend", th)
+                    } else if (i == 1) {
+                        th.insertAdjacentText("afterbegin", map.get(itera)[0]);
+                        row.insertAdjacentElement("beforeend", th)
+                    } else if (i == 2) {
+                        th.insertAdjacentText("afterbegin", map.get(itera)[1]);
+                        row.insertAdjacentElement("beforeend", th)
+                    } else if (i == 3) {
+                        th.insertAdjacentText("afterbegin", map.get(itera)[2]);
+                        row.insertAdjacentElement("beforeend", th)
+                    } else if (i == 4) {
+                        th.insertAdjacentText("afterbegin", map.get(itera)[3]);
+                        row.insertAdjacentElement("beforeend", th)
+                    } else {
+                        th.insertAdjacentText("afterbegin", map.get(itera)[4]);
+                        row.insertAdjacentElement("beforeend", th)
+                        break
+                    }
+                }
+                tbody.insertAdjacentElement("afterbegin", row)
+                table.insertAdjacentElement("beforeend", tbody)
+            } else {
+                bdd[0] -= capital;
+                bdd[1] -= deuda_mensual;
+                let bdd_copia = bdd.slice();
+                map.set(itera, bdd_copia);
+                let tbody = document.querySelector("tbody")
+                let row = document.createElement("tr");
+                for (let i = 0; i < 6; i++) {
+                    let th = document.createElement("th");
+                    if (i == 0) {
+                        th.insertAdjacentText("afterbegin", itera);
+                        row.insertAdjacentElement("beforeend", th)
+                    } else if (i == 1) {
+                        th.insertAdjacentText("afterbegin", map.get(itera)[0]);
+                        row.insertAdjacentElement("beforeend", th)
+                    } else if (i == 2) {
+                        th.insertAdjacentText("afterbegin", map.get(itera)[1]);
+                        row.insertAdjacentElement("beforeend", th)
+                    } else if (i == 3) {
+                        th.insertAdjacentText("afterbegin", map.get(itera)[2]);
+                        row.insertAdjacentElement("beforeend", th)
+                    } else if (i == 4) {
+                        th.insertAdjacentText("afterbegin", map.get(itera)[3]);
+                        row.insertAdjacentElement("beforeend", th)
+                    } else {
+                        th.insertAdjacentText("afterbegin", map.get(itera)[4]);
+                        row.insertAdjacentElement("beforeend", th)
+                        break
+                    }
+                }
+                tbody.insertAdjacentElement("afterbegin", row)
+                table.insertAdjacentElement("beforeend", tbody)
+            }
+        };
+        fragmento.insertAdjacentElement("afterbegin", table)
+        tabla2.insertAdjacentElement("afterbegin", fragmento)
+    };
+    tabla2(prestamo, total_deuda, capital, interes_m, interes_a);
 }
 
-const input_prestamo = Number(prompt("Simulador de prestamo\n\nMonto del credito"));
-const input_interes = Number(prompt("Interés% (Anual)"));
-const input_pagos = Number(prompt("Número de pagos (Mensuales)"));
+const input_prestamo = document.querySelector("#prestamo");
+const input_interes = document.querySelector("#intereses");
+const input_pagos = document.querySelector("#pagos");
 
-///Escritura en html
-
-
-
-
-proceso(input_prestamo, input_interes, input_pagos)
+proceso(input_prestamo.value, input_interes.value, input_pagos.value)
