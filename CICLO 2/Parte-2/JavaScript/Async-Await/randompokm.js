4cdc80d8a13710712efb54a0bcc2b8ea92534a2e
Johan González <soyelsxbbsx@outlook.com>
2023-12-30 16:51:07 -0500

    Pokemones consecutivos, aleatorios

--- CICLO 2/Parte-2/JavaScript/Async-Await/randompokm.js
+++ CICLO 2/Parte-2/JavaScript/Async-Await/randompokm.js
@@ -1,11 +1,8 @@
-//CREAR UN MARCABLE QUE ALTERNE ENTRE LAS 2 OPCIONES
-
-// Opcion aleatoria
 function toCapitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-function idSelect(arr) {
+async function idSelect(arr) {
     let id = Math.floor(Math.random() * 1000) + 1;
     while (!arr.indexOf(id)) {
         id = Math.floor(Math.random() * 1000) + 1;
@@ -14,27 +11,89 @@ function idSelect(arr) {
     return id
 }
 
-const d = document;
-let descartes = [];
-const switcher = () => {
-    debugger
-    const id = idSelect(descartes);
-    const imgplace1 = d.querySelector("box1");
-    const idplace1 = d.querySelector("id");
-    const nameplace1 = d.querySelector("name");
-    const url = "https://pokeapi.co/api/v2/pokemon/";
-    fetch(`${url}${id}`)
-        .then(res => res.json())
-        .then(pokemon => {
-            let name = pokemon.forms[0].name;
-            name = toCapitalize(name);
-            nameplace1.insertAdjacentText("afterbegin", name)
-            idplace1.insertAdjacentText("afterbegin", id)
-            const sprite = pokemon.sprites.front_default;
-            imgplace1.setAttribute("src", sprite);
-        })
+async function OrdIdSelect(arr, i) {
+    while (arr.indexOf(i) !== -1) {
+        i++;
+    };
+    arr.push(i);
+    return i
 }
 
-
-// Opcion consecutiva
-
+const d = document;
+let descartes = [];
+const switcher = async () => {
+    debugger
+    const check = d.querySelector("#check");
+    if (check.checked) {
+        // Opcion aleatoria
+        let id = await idSelect(descartes);
+        let imgplace1 = d.querySelector(".box1");
+        let idplace1 = d.querySelector("#id1");
+        idplace1.textContent = "";
+        let nameplace1 = d.querySelector("#name1");
+        nameplace1.textContent = "";
+        const url = "https://pokeapi.co/api/v2/pokemon/";
+        await fetch(`${url}${id}`)
+            .then(res => res.json())
+            .then(pokemon => {
+                let name = pokemon.forms[0].name;
+                name = toCapitalize(name);
+                nameplace1.insertAdjacentText("afterbegin", name)
+                idplace1.insertAdjacentText("afterbegin", `#${id}`)
+                const sprite = pokemon.sprites.front_default;
+                imgplace1.setAttribute("src", sprite);
+            });
+        id = await idSelect(descartes);
+        let imgplace2 = d.querySelector(".box2");
+        let idplace2 = d.querySelector("#id2");
+        idplace2.textContent = "";
+        let nameplace2 = d.querySelector("#name2");
+        nameplace2.textContent = "";
+        fetch(`${url}${id}`)
+            .then(res => res.json())
+            .then(pokemon => {
+                let name = pokemon.forms[0].name;
+                name = toCapitalize(name);
+                nameplace2.insertAdjacentText("afterbegin", name)
+                idplace2.insertAdjacentText("afterbegin", `#${id}`)
+                const sprite = pokemon.sprites.front_default;
+                imgplace2.setAttribute("src", sprite);
+            });
+    } else {
+        // Opcion consecutiva
+        let id = 1;
+        id = await OrdIdSelect(descartes, id);
+        let imgplace1 = d.querySelector(".box1");
+        let idplace1 = d.querySelector("#id1");
+        idplace1.textContent = "";
+        let nameplace1 = d.querySelector("#name1");
+        nameplace1.textContent = "";
+        const url = "https://pokeapi.co/api/v2/pokemon/";
+        await fetch(`${url}${id}`)
+            .then(res => res.json())
+            .then(pokemon => {
+                let name = pokemon.forms[0].name;
+                name = toCapitalize(name);
+                nameplace1.insertAdjacentText("afterbegin", name)
+                idplace1.insertAdjacentText("afterbegin", `#${id}`)
+                const sprite = pokemon.sprites.front_default;
+                imgplace1.setAttribute("src", sprite);
+            });
+        id = await OrdIdSelect(descartes, id);
+        let imgplace2 = d.querySelector(".box2");
+        let idplace2 = d.querySelector("#id2");
+        idplace2.textContent = "";
+        let nameplace2 = d.querySelector("#name2");
+        nameplace2.textContent = "";
+        fetch(`${url}${id}`)
+            .then(res => res.json())
+            .then(pokemon => {
+                let name = pokemon.forms[0].name;
+                name = toCapitalize(name);
+                nameplace2.insertAdjacentText("afterbegin", name)
+                idplace2.insertAdjacentText("afterbegin", `#${id}`)
+                const sprite = pokemon.sprites.front_default;
+                imgplace2.setAttribute("src", sprite);
+            });
+    }
+}
